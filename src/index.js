a5a7a0942406c92fa864ec1d5e0092a81ed74506
okoro <mi.fracri91@gmail.com>
2025-11-10 17:24:37 +0100

    structure steup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./style.css";
+console.log("welcome to my restaurant page");
+
+const heroHeading = document.createElement("h1");
+heroHeading.textContent = "Welcome to My Restaurant";
+document.getElementById("content").appendChild(heroHeading);
